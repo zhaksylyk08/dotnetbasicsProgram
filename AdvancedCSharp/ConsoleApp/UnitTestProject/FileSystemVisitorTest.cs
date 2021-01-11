be90b9191921d711260047a6685f78aafbfdccdc
Zhadyra Zhaksylyk <Zhadyra_Zhaksylyk@epam.com>
2021-01-11 10:28:04 +0600

    Added TestMethod

--- AdvancedCSharp/ConsoleApp/UnitTestProject/FileSystemVisitorTest.cs
+++ AdvancedCSharp/ConsoleApp/UnitTestProject/FileSystemVisitorTest.cs
@@ -1,15 +1,29 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
+using ConsoleApp;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class FileSystemVisitorTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        private FileSystemVisitor _fileSystemVisitor;
 
+        [SetUp]
+        public void SetUp()
+        {
+            string rootPath = @"C:\dotnetmentoring";
+            _fileSystemVisitor = new FileSystemVisitor(rootPath);
+        }
+
+        [TestMethod]
+        public void InvalidDirectoryPath_ThrowsException()
+        {
+            var invalidRootPath = @"C:\dotnetmentoring222";
+            var fileSystemVisitor = new FileSystemVisitorTest(invalidRootPath);
+
+            Assert.Throws<Exception>(() => fileSystemVisitor.TraverseFileSystem());
         }
     }
 }
