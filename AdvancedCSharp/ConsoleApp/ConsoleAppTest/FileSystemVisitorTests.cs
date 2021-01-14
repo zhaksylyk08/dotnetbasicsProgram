89561a7f9961a6d0df072857141dd05a1e988e26
Zhadyra Zhaksylyk <Zhadyra_Zhaksylyk@epam.com>
2021-01-14 21:25:51 +0600

    Added Test Method

--- AdvancedCSharp/ConsoleApp/ConsoleAppTest/FileSystemVisitorTests.cs
+++ AdvancedCSharp/ConsoleApp/ConsoleAppTest/FileSystemVisitorTests.cs
@@ -10,19 +10,27 @@ namespace ConsoleAppTest
     {
         private FileSystemVisitor _fileSystemVisitor;
         private string _testPath;
+        private int _numberOfSubDirs;
+        private int _numberofFiles;
 
         [SetUp]
         public void Setup()
         {
             _testPath = Path.Combine(Path.GetTempPath(), "test");
+            _numberOfSubDirs = 4;
+            _numberofFiles = 5;
 
             if (!Directory.Exists(_testPath))
             {
                 var di = new DirectoryInfo(_testPath);
 
-                for (int i = 1; i < 4; i++)
+                for (int i = 0; i < _numberOfSubDirs; i++)
                 {
                     di.CreateSubdirectory(String.Concat("dir", i));
+                }
+
+                for (int i = 0; i < _numberofFiles; i++)
+                {
                     File.Create(Path.Combine(_testPath, String.Concat("file", i)));
                 }
             }
@@ -36,6 +44,20 @@ namespace ConsoleAppTest
             Assert.IsInstanceOf<IEnumerable>(_fileSystemVisitor);
         }
 
+        [Test]
+        public void TraverseFileSystem_Returns_AllSubDirsAndFiles()
+        {
+            var di = new DirectoryInfo(_testPath);
+            int expectedNumberOfItems = di.GetFileSystemInfos().Length;
 
+            int actualNumberOfItems = 0;
+
+            foreach (var item in _fileSystemVisitor)
+            {
+                actualNumberOfItems += 1;
+            }
+
+            Assert.AreEqual(expectedNumberOfItems, actualNumberOfItems);
+        }
     }
 }
