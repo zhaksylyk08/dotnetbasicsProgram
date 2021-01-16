b993ee8fa896216c9211f726bb13185177d190c4
Zhadyra Zhaksylyk <Zhadyra_Zhaksylyk@epam.com>
2021-01-16 14:41:41 +0600

    Added Exception Handling HW 1

---
+++ ExceptionHandling/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To terminate the program, enter exit.");
+
+            while (true)
+            {
+                Console.WriteLine("Enter the line:");
+
+                var input = Console.ReadLine();
+
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                try
+                {
+                    var result = input[0];
+                    Console.WriteLine(result);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("You entered empty line.");
+                }
+            }
+        }
+    }
+}
