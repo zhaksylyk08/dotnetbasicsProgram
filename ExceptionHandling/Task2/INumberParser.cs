24391d1dd642cdf720d6dcf79af7eacb05e9adac
Zhadyra Zhaksylyk <Zhadyra_Zhaksylyk@epam.com>
2021-01-18 12:39:00 +0600

    Added Task2, Task2.Tests

---
+++ ExceptionHandling/Task2/INumberParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    public interface INumberParser
+    {
+        int Parse(string stringValue);
+    }
+}
---
+++ ExceptionHandling/Task2/NumberParser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            int result = 0;
+            int degree = 0;
+            int sum = 0;
+
+            checked
+            {
+                try
+                {
+                    if (stringValue.Length == 0)
+                    {
+                        throw new FormatException($"Length of string number is 0");
+                    }
+
+                    for (int i = stringValue.Length - 1; i >= 0; i--)
+                    {
+                        while (stringValue[i] == ' ')
+                        {
+                            i--;
+
+                            if (i == -1)
+                            {
+                                throw new FormatException("Number string is empty");
+                            }
+                        }
+
+                        var currentChar = stringValue[i];
+                        var currentDigit = (int)Char.GetNumericValue(stringValue[i]);
+
+                        if (currentDigit == -1)
+                        {
+                            if (i == 0)
+                            {
+                                if (currentChar == '-')
+                                {
+                                    result *= -1;
+                                }
+
+                                break;
+                            }
+                            else
+                            {
+                                throw new FormatException($"{stringValue} is invalid number string");
+                            }
+                        }
+
+                        sum = currentDigit * (int)Math.Pow(10, degree);
+                        result += sum;
+                        degree++;
+                    }
+
+                    return result;
+                }
+                catch (NullReferenceException e)
+                {
+                    throw new ArgumentNullException("stringValue argument is null", e);
+                }
+                catch (OverflowException)
+                {
+                    double temp = (result + sum) * -1;
+                    double minValue = int.MinValue;
+
+                    if (temp == minValue)
+                    {
+                        return int.MinValue;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
