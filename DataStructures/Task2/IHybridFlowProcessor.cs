c0daebfcdc34954c04d0ce86046f76a22cef50e7
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2021-01-31 05:32:29 +0600

    Added Data structures Task 2

---
+++ DataStructures/Task2/IHybridFlowProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    public interface IHybridFlowProcessor<T>
+    {
+        void Push(T item);
+        T Pop();
+        void Enqueue(T item);
+        T Dequeue();
+    }
+}
