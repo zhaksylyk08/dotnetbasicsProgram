5199681ab02e89c409840c351fa7737aa7799d56
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2021-02-15 00:30:53 +0600

    Added Negative Numbers Test Cases

--- UnitTesting/TestProject/StringSumTests.cs
+++ UnitTesting/TestProject/StringSumTests.cs
@@ -18,6 +18,9 @@ namespace TestProject
         [TestCase("2", " ", ExpectedResult = "2")]
         [TestCase("3.5", "1", ExpectedResult = "1")]
         [TestCase("23", "0.5", ExpectedResult = "23")]
+        [TestCase("-15", "7", ExpectedResult = "7")]
+        [TestCase("8", "-1", ExpectedResult = "8")]
+        [TestCase("-55", "-12", ExpectedResult = 0)]
         public string Sum_Returns_StringsSum(string num1, string num2)
         {
             return _stringSum.Sum(num1, num2);
