7ed1c658dac6218a77307faf195de38dd0e03266
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2021-02-15 03:47:03 +0600

    Added CalcStats, CalcStatsTests

---
+++ UnitTesting/Katas/CalcStats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    public class CalcStats
+    {
+        private int[] _numbers;
+        public int Min { get; private set; }
+
+        public int Max { get; private set; }
+
+        public double Average { get; private set; }
+
+        public int NumberOfElements { get; private set; }
+
+        public CalcStats(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException("Array is empty");
+            }
+
+            _numbers = numbers;
+
+            Min = _numbers.Min();
+            Max = _numbers.Max();
+            Average = _numbers.Average();
+            NumberOfElements = _numbers.Length;
+        }
+    }
+}
---
+++ UnitTesting/TestProject/CalcStatsTests.cs
@@ -0,0 +1,35 @@
+﻿using Katas;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TestProject
+{
+    public class CalcStatsTests
+    {
+        private CalcStats _calcStats;
+        private int[] _arr;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _arr = new int[] { 6, 9, 15, -2, 92, 11 };
+
+            _calcStats = new CalcStats(_arr);
+        }
+
+        [Test]
+        public void CalcStats_NonEmptyArray()
+        {
+            int expectedMin = _arr.Min();
+            int expectedMax = _arr.Max();
+            int expectedNumberOfElements = _arr.Length;
+            double expectedAverage = _arr.Average();
+
+            Assert.AreEqual(expectedMin, _calcStats.Min);
+            Assert.AreEqual(expectedMax, _calcStats.Max);
+            Assert.AreEqual(expectedNumberOfElements, _calcStats.NumberOfElements);
+            Assert.AreEqual(expectedAverage, _calcStats.Average);
+        }
+    }
+}
